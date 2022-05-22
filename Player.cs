27e376d349f187d6cd6d239dc9870a5faa53528e
JJ Ross <jeffreysrossjr@gmail.com>
2022-05-22 14:52:07 -0400

    begin implementing Player class

--- Player.cs
+++ Player.cs
@@ -1,9 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net.Sockets;
 
 
 class Player
 {
+    private List<Card> hand = new List<Card>();
+    private Socket connection;
+
+    public Player(Socket connection)
+    {
+        this.connection = connection;
+    }
+
+    public int GetTotal()
+    {
+        int total = 0;
+        foreach (Card card in hand)
+        {
+            total += card.Value;
+        }
+        return total;
+    }
 }
 
